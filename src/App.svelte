043b8db4a81c7ae9645c1a9210460ad1656ea9e8
Chris Hanagan <chris.hanagan@gmail.com>
2024-12-19 16:35:41 -0500

    starting point

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Secondary from "./secondary.svelte";
+	let count = 10;
+
+	let box = {
+		x: 5,
+		y: 10
+	};
+	
+</script>
+
+
+<button on:click={() => count++}>+</button>
+<Secondary {count} {...box} />
---
+++ src/Secondary.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let count = 0;
+    export let x = 0;
+    export let y = 0;
+</script>
+
+<h1>{count}</h1>
+
+<h2>x: {x} y: {y} = {x + y} </h2>
+
+
